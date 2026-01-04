bf30a3b226479d9deab3fe6f6d4e1433a8b079c0
nk4110803 <nk4110803@gmail.com>
2026-01-04 15:37:06 +0200

    Implementing a clean solution for the exercise using factories and tests 04.01.26

--- CalculatorApp/Calculator.cs
+++ CalculatorApp/Calculator.cs
@@ -30,50 +30,6 @@ namespace src
             }
             return int.Parse(s);
         }
-
-        //public int Add(string s)
-        //{
-        //    string[]parts= cleanInput(s).Split('+');
-        //    int a=int.Parse (parts[0]);
-        //    int b=int.Parse (parts[1]);
-        //    return a + b;
-        //}
-        //public int Subtraction(string s)
-        //{
-        //    string[] parts = cleanInput(s).Split('-');
-        //    int a = int.Parse(parts[0]);
-        //    int b = int.Parse(parts[1]);
-        //    return a - b;
-        //}
-        //public int multiplication(string s)
-        //{
-
-        //    string[] parts = cleanInput(s).Split('*');
-        //    int a = int.Parse(parts[0]);
-        //    int b = int.Parse(parts[1]);
-        //    return a * b;
-        //}
-        //public int Division(string s)
-        //{
-
-        //    string[] parts = cleanInput(s).Split('/');
-        //    int b = int.Parse(parts[1]);
-        //    if (b == 0)
-        //    {
-        //        throw new DivideByZeroException();
-        //    }
-        //    int a = int.Parse(parts[0]);
-        //    return a / b;
-        //}
-
-        //private string cleanInput(string s)
-        //{
-        //    if (string.IsNullOrEmpty(s))
-        //    {
-        //        return "";
-        //    }
-        //    return s.Replace(" ", "");
-        //}
     }
 
 }
